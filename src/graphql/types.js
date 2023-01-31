29d2ebd7ce824e39d87b709deffdca0919d16dd9
unknown <tylermiller1321@outlook.com>
2023-01-31 15:45:05 -0600

    add a querie to get a single user by id

--- src/graphql/types.js
+++ src/graphql/types.js
@@ -3,6 +3,7 @@ const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql')
 
 const UserType = new GraphQLObjectType (
     {
+        
         name: 'UserType',
         fields: () => ({
             id: {type: GraphQLID},
