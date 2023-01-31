e9659099199e4c69f83b28bfee5e43a77b18d658
unknown <tylermiller1321@outlook.com>
2023-01-31 12:27:05 -0600

    adding userSchema and model, add user to db on register

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            unique: true, 
+            require: true
+        },
+        email:{
+            type: String,
+            unique: true,
+            require: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?)*(\.\w{2,3})+$/,
+                'Please enter a valid email'
+            ]
+        },
+        password:{
+            type: String,
+            require: true            
+        }
+    },
+    {timestamps: true}
+)
+
+
+module.exports = mongoose.model("User", userSchema);
